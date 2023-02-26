614fd23ddcc43294cbea39c081a337d5ffbe7aea
Jakob Brennan <jakob.brennan@gmail.com>
2023-02-26 19:38:07 +0000

    CSS beautification

--- assets/js/cats.js
+++ assets/js/cats.js
@@ -1,75 +1,106 @@
+
 const questions = [
     { 
+      image: 'assets/imgs/cat1.jpg',
       question: "How cute is your cat?",
       answers: ["Very", "Incredibly", "A bit", "Not cute"],
       answer: 1
     },
     {
+      image: 'assets/imgs/cat2.jpg',
       question: "Does your cat like chicken?", 
       answers: ["yes", "no", "no comment", "don't know"],
       answer: 0
     },
     {
+      image: 'assets/imgs/cat3.jpg',
       question: "Where is your cat?",
       answers: ["asleep on the couch", "on top of the fridge", "climbing out the window", "that's top secret info"],
       answer: 2
     },
     {
+      image: 'assets/imgs/cat4.jpg',
       question: "What is your cat thinking about?", 
       answers: ["mischief", "food", "sleep", "birds"],
       answer: 0
     },
     { 
+      image: 'assets/imgs/cat5.jpg',
       question: "Can your cat open doors?",
       answers: ["Don't know", "I hope not", "How could he?", "Yes"],
       answer: 3
     },
     {
+      image: 'assets/imgs/cat6.jpg',
       question: "Does your cat love cheese?", 
       answers: ["yes", "no", "no comment", "don't know"],
       answer: 0
     },
     {
+      image: 'assets/imgs/cat7.jpg',
       question: "How many toe beans does a cat have?",
       answers: ["Five", "Six", "Twenty", "what's a toe bean?"],
       answer: 2
     },
     {
+      image: 'assets/imgs/cat8.jpg',
       question: "Did you know your cat is behind you?", 
       answers: ["What now?", "No", "Yes", "*eerie silence*"],
       answer: 1
+    },
+    {
+      image: '',
+      question: "",
+      answers: "",
+      answer: ""
     }
- ]; //add more questions here
-var qText = document.getElementById('question');
-var aNswer = document.getElementsByClassName('answer');
+ ];
+
+
+let qText = document.getElementById('question');
+let qImg = document.getElementById('qImage');
+let aNswer = document.getElementsByClassName('answer');
 let btn = document.getElementById('next');
-let submitButton =  document.getElementById('submit');
-submitButton.addEventListener('click',submit);
-//btn.addEventListener('click', showNext);
-var answersList = document.getElementsByClassName("answerFields");
+let answersList = document.getElementsByClassName("answerFields");
+let verify = document.getElementById('verify');
 let quizProgress =  0;
 var i = 0;
+verify.style.display = "none";
+document.getElementById('scoreValues').style.display = 'none';
 
-btn.onclick = function() { 
-   //if(quizProgress>questions.length -1){   
+function hideButtons() {
+   for(let i = 0; i < answersList.length; i++ ) {
+      answersList[i].style.display = 'none';
+   }
+}
+
+hideButtons();
+
+window.onload = function() {  
    showNext(quizProgress);
+   btn.style.display = 'none';
 };
 
 // qText.innerText = questionText;
 
 function showNext() {
    var aQuestion = questions[quizProgress];
-   qText.innerText = aQuestion.question;
-
+   let br = document.createElement("br");
+   qText.innerText = ("Question " + (quizProgress+1) + ": " + aQuestion.question);
+   qImg.src = aQuestion.image;
    for(let i = 0; i < answersList.length; i++ ) {
+      answersList[i].style.display = '';
       answersList[i].textContent = aQuestion.answers[i];
       answersList[i].setAttribute("data-type", (i));
-   }
-   if (quizProgress == 8) {
-      calcResults();
-   }
-   else { 
-      listenAnswer(); 
+
+      if (quizProgress === 8) {
+         calcResults();
+         hideButtons();
+         qText.style.display = 'none';
+      }
+      else { 
+         listenAnswer(); 
+      }
    }
 }
 
@@ -82,12 +113,25 @@ function listenAnswer() {
 
 var score = 0;
 function verifyAnswer(e) {
+   const delay = setTimeout(next, 400);
+   function next() {
+      verify.style.display = "none";
+      ++quizProgress;
+      console.log("new question is:");
+      console.log(quizProgress+1);
+      showNext(quizProgress); }
+ 
    var aQuestion = questions[quizProgress];
    if (e.target.getAttribute("data-type") == aQuestion.answer) {
       console.log("correct");
       ++score;
       console.log("score is:" );
       console.log(score);
+      let correct = "Correct! Well done."
+      verify.style.display = "";
+      verify.style.backgroundColor = "green";
+      verify.innerText = correct;
+      delay;
    }
    else if (e.target.getAttribute("data-type") != aQuestion.answer) {
       console.log("incorrect! you picked:")
@@ -96,6 +140,11 @@ function verifyAnswer(e) {
       console.log(aQuestion.answer);
       console.log("score is:" );
       console.log(score);
+      let incorrect = "100% wrong, sorry!"
+      verify.style.display = "";
+      verify.style.backgroundColor = "red";
+      verify.innerText = incorrect;
+      delay;
    };
    qText.innerHTML = "";
    answersList.textContent = "";
@@ -103,14 +152,7 @@ function verifyAnswer(e) {
    console.log("old question was:");
    console.log(quizProgress+1);
 
-   if (quizProgress === 7) {
-      calcResults();
-   }
-   else { 
-      ++quizProgress;
-      console.log("new question is:");
-      console.log(quizProgress+1);
-      showNext(quizProgress); }
+  
  
    
 }
@@ -121,18 +163,26 @@ function verifyAnswer(e) {
 
 function calcResults() 
 {
+  let ansDiv = document.getElementById('answerList');
+  var catResultImg = document.getElementById('resultsCat');
   var pass = "your cat knowledge isn't very good";
   var distinction = "you could do with brushing up on cats";
   var merit = "you're a proper cat genius";
   var showResultText = document.getElementById('resultsText');
+  document.getElementById('scoreValues').style.display = '';
   var results = parseInt(score);
   var rawValue = document.getElementById('rawvalue');
   var percentage = document.getElementById('percent');
   const calcScore = (results / 8) * 100;
   rawValue.innerText = results;
   percentage.innerText = calcScore;
-  if (results <=3) {showResultText.innerText = pass; };
-  if (results <=6) {showResultText.innerText = distinction;};
-  if (results >=7) {showResultText.innerText = merit;};
+  ansDiv.style.display = "none";
+  if (results <=3) {showResultText.innerText = pass; catResultImg.src ="assets/imgs/angry.gif"; btn.style.display = ''; btn.innerText = "Restart Quiz"; };
+  if (results >=4) {showResultText.innerText = distinction; catResultImg.src ="assets/imgs/fall.gif"; btn.style.display = ''; btn.innerText = "Restart Quiz"; };
+  if (results >=7) {showResultText.innerText = merit; catResultImg.src ="assets/imgs/smile.gif"; btn.style.display = ''; btn.innerText = "Restart Quiz"; };
+  btn.onclick = function() {
+   location.reload();
+   btn.innerText = "Start Quiz";
+   btn.style.display = "none"; }
   // html correspond = <p>Congrats! You got <span id="rawvalue"></span> out of 8. In other words, you scored <span id=percent></span>. </p>
 }
