ece473032a32edb23c3b8ac001f1a11571e8a281
Jakob Brennan <jakob.brennan@gmail.com>
2023-03-04 21:08:40 +0000

    more imgs, css polishing, chng to SessionStorage

--- assets/js/cats.js
+++ assets/js/cats.js
@@ -1,49 +1,49 @@
 
 const questions = [
     { 
-      image: 'assets/imgs/cat1.jpg',
+      image: 'assets/imgs/evie1.webp',
       question: "How cute is your cat?",
       answers: ["Very", "Incredibly", "A bit", "Not cute"],
       answer: 1
     },
     {
-      image: 'assets/imgs/cat2.jpg',
+      image: 'assets/imgs/mrman.webp',
       question: "Does your cat like chicken?", 
       answers: ["yes", "no", "no comment", "don't know"],
       answer: 0
     },
     {
-      image: 'assets/imgs/cat3.jpg',
+      image: 'assets/imgs/evie2.webp',
       question: "Where is your cat?",
       answers: ["asleep on the couch", "on top of the fridge", "climbing out the window", "that's top secret info"],
       answer: 2
     },
     {
-      image: 'assets/imgs/cat4.jpg',
+      image: 'assets/imgs/mrman2.webp',
       question: "What is your cat thinking about?", 
       answers: ["mischief", "food", "sleep", "birds"],
       answer: 0
     },
     { 
-      image: 'assets/imgs/cat5.jpg',
+      image: 'assets/imgs/evie3.webp',
       question: "Can your cat open doors?",
       answers: ["Don't know", "I hope not", "How could he?", "Yes"],
       answer: 3
     },
     {
-      image: 'assets/imgs/cat6.jpg',
+      image: 'assets/imgs/mrman3.webp',
       question: "Does your cat love cheese?", 
       answers: ["yes", "no", "no comment", "don't know"],
       answer: 0
     },
     {
-      image: 'assets/imgs/cat7.jpg',
+      image: 'assets/imgs/eviemrman.webp',
       question: "How many toe beans does a cat have?",
       answers: ["Five", "Six", "Twenty", "what's a toe bean?"],
       answer: 2
     },
     {
-      image: 'assets/imgs/cat8.jpg',
+      image: 'assets/imgs/eviemrman2.webp',
       question: "Did you know your cat is behind you?", 
       answers: ["What now?", "No", "Yes", "*eerie silence*"],
       answer: 1
@@ -57,17 +57,9 @@ const questions = [
  ];
 
 
-function fromStorage() {
-   var savedUser = localStorage.usrName;
-   var savedScore = localStorage.scoreValue;
-      var table = document.getElementById('highscores');
-      let row = table.insertRow();
-      let td1 = row.insertCell(0);
-      let td2 = row.insertCell(1);
-      td1.innerText = savedUser;
-      td2.innerText = savedScore;
-}
 
+
+let table = document.getElementById('highscores');
 let qText = document.getElementById('question');
 let qImg = document.getElementById('qImage');
 let aNswer = document.getElementsByClassName('answer');
@@ -78,6 +70,21 @@ let quizProgress =  0;
 var i = 0;
 verify.style.display = "none";
 document.getElementById('scoreValues').style.display = 'none';
+
+function fromStorage() {
+   var savedUser = sessionStorage.usrName;
+   var savedScore = sessionStorage.scoreValue;
+   console.log(sessionStorage.usrName);
+   console.log("test");
+   console.log(sessionStorage.scoreValue);
+   console.log("test1");
+      let row = table.insertRow();
+      let td1 = row.insertCell(0);
+      let td2 = row.insertCell(1);
+      td1.innerText = savedUser;
+      td2.innerText = savedScore;
+}
+fromStorage();
 
 function hideButtons() {
    for(let i = 0; i < answersList.length; i++ ) {
@@ -97,7 +104,8 @@ window.onload = function() {
 function showNext() {
    var aQuestion = questions[quizProgress];
    let br = document.createElement("br");
-   qText.innerText = ("Question " + (quizProgress+1) + ": " + aQuestion.question);
+   console.log(questions.length);
+   qText.innerText = ("Question " + (quizProgress+1) + "/8: " + aQuestion.question);
    qImg.src = aQuestion.image;
    for(let i = 0; i < answersList.length; i++ ) {
       answersList[i].style.display = '';
@@ -139,8 +147,9 @@ function verifyAnswer(e) {
       console.log("score is:" );
       console.log(score);
       let correct = "Correct! Well done."
+      verify.style.backgroundColor = "rgba(0, 156, 0, 35%)";
+      verify.style.animation = "fadeIn 0.3s";
       verify.style.display = "";
-      verify.style.backgroundColor = "green";
       verify.innerText = correct;
       delay;
    }
@@ -152,8 +161,9 @@ function verifyAnswer(e) {
       console.log("score is:" );
       console.log(score);
       let incorrect = "100% wrong, sorry!"
+      verify.style.backgroundColor = "rgba(208, 0, 0, 35%)";
+      verify.style.animation = "fadeIn 0.3s";
       verify.style.display = "";
-      verify.style.backgroundColor = "red";
       verify.innerText = incorrect;
       delay;
    };
@@ -162,6 +172,7 @@ function verifyAnswer(e) {
    answersList.setAttribute = "";
    console.log("old question was:");
    console.log(quizProgress+1);
+   table.style.display = "none";
 
   
  
@@ -200,9 +211,10 @@ function calcResults()
 
 
 function submitScore() {
+   alert("Score added! Scroll to see the all-time highscores.");
+   table.style.display = "";
    var usr = document.getElementById('user').value;
    var rawValue = score;
-   var table = document.getElementById('highscores');
    console.log(rawValue);
    console.log (usr);
    let row = table.insertRow();
@@ -210,8 +222,6 @@ function submitScore() {
    let td2 = row.insertCell(1);
    td1.innerText =  usr;
    td2.innerText = rawValue;
-   localStorage.usrName = usr;
-   localStorage.scoreValue = rawValue;
-   console.log(localStorage.usrName);
-   console.log(localStorage.scoreValue);
+   sessionStorage.usrName = usr;
+   sessionStorage.scoreValue = rawValue;
 }
